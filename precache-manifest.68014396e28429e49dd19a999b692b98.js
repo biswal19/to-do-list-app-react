1cfac4138277217940d9368256e358aaa66707e3
santos <biswal14@yahoo.com>
2021-03-18 17:22:32 +0530

    Updates

---
+++ precache-manifest.68014396e28429e49dd19a999b692b98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe2294bd3d2f947d2dcd4788260d32b2",
+    "url": "/to-do-list-app-react/index.html"
+  },
+  {
+    "revision": "2c2d2919c106e69569af",
+    "url": "/to-do-list-app-react/static/css/main.f171ecbb.chunk.css"
+  },
+  {
+    "revision": "27130051255a819fa7ea",
+    "url": "/to-do-list-app-react/static/js/2.7bd08d42.chunk.js"
+  },
+  {
+    "revision": "2c2d2919c106e69569af",
+    "url": "/to-do-list-app-react/static/js/main.11ab4e94.chunk.js"
+  },
+  {
+    "revision": "66eb29c1fca9b0fff384",
+    "url": "/to-do-list-app-react/static/js/runtime~main.44f9f0de.js"
+  }
+]);
